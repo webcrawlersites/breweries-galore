c6b2cb54ec443d188e9367c008388e37dbf9648c
Jason Bischak <webcrawlersites@gmail.com>
2022-04-04 14:36:46 -0400

    Show search results added

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,24 +1,43 @@
-// const searchBtn = document.getElementById('recipe-search-button');
+const searchBtn = document.getElementById('beer-search-button');
+const breweryContainer = document.getElementById('brewery-container');
 
-// const getRecipeData = (ingredient) => {
-//     const recipeApi = `https://api.spoonacular.com/recipes/findByIngredients?apiKey=19d876a131904f91975514c3c6880781&ingredients=${ingredient}`;
+const getBeerData = (cityName) => {
+    const beerApi = `https://api.openbrewerydb.org/breweries?by_city=${encodeURIComponent(cityName)}`;
+    fetch(beerApi)
+    .then(res => {
+        if(res.ok) {
+            return res.json();
+        } else {
+            console.log('Error!');
+        }
+    })
+    .then(res => {
+        console.log(res);
+        res.forEach(brewery => {
+            createBreweryCard(brewery.name, brewery.street, brewery.city, brewery.postal_code, brewery.website_url, brewery.phone);
+        });
+    });
+}
 
-//     fetch(recipeApi)
-//     .then(res => {
-//         if(res.ok) {
-//             console.log(res.json());
-//         } else {
-//             console.log('Error!');
-//         }
-//     })
-//     .then(data => {
-//         return console.log(data);
-//     });
-// }
+const createBreweryCard = (name, street, city, zip, website, phone) => {
+    let breweryCard = `
+    <div class="brewery-card">
+        <h2>${name}</h2>
+        <ul>
+            <li><span class="info-title">Address:</span> ${street}, ${city}, ${zip}</li>
+            <li><span class="info-title">Phone Number:</span> ${phone == null || phone == undefined ? 'n/a' : phone}</li>
+            <li><span class="info-title">Website:</span> ${website == null || website == undefined ? 'n/a' : website}</li>
+        </ul>
+    </div>
+    `;
+    breweryContainer.insertAdjacentHTML('beforeend', breweryCard);
 
-// searchBtn.addEventListener('click', function(e) {
-//     e.preventDefault;
-//     let searchQuery = document.getElementById('recipe-input').value;
+}
+
+searchBtn.addEventListener('click', function(e) {
+    e.preventDefault;
+    breweryContainer.innerHTML = '';
+    let searchQuery = document.getElementById('city-input').value;
     
-//     getRecipeData(searchQuery);
-// });
+    getBeerData(searchQuery);
+});
